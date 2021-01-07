a7484b0484fafd26d38e511337dfb73cb6e84307
shaneguther <shaneguther@gmail.com>
2021-01-07 14:46:30 -0500

    Fixed formatting issue with record.xml and recyclerview, added visual representation of tee color to past records.

--- app/src/main/java/com/example/golfscore/MyAdapter.kt
+++ app/src/main/java/com/example/golfscore/MyAdapter.kt
@@ -1,6 +1,9 @@
 package com.example.golfscore
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Color.parseColor
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,7 +31,17 @@ class MyAdapter(val data : ArrayList<Scorecard>, var context: Context) : Recycle
         holder.itemView.tv_courseEntry.text = data[position].courseName
         holder.itemView.tv_parEntry.text = data[position].par.toString()
         holder.itemView.tv_scoreEntry.text = data[position].score.toString()
-        holder.itemView.tv_teeEntry.text = data[position].tees
+        val teeColor = data[position].tees
+
+        if(teeColor.equals("Black")){
+            holder.itemView.tv_teeEntry.setBackgroundColor(Color.BLACK)
+        }else if(teeColor.equals("Blue")){
+            holder.itemView.tv_teeEntry.setBackgroundColor(parseColor("#2172ff"))
+        }else if(teeColor.equals("White")){
+            holder.itemView.tv_teeEntry.setBackgroundColor(Color.WHITE)
+        }else if(teeColor.equals("Red")){
+            holder.itemView.tv_teeEntry.setBackgroundColor(parseColor("#F10404"))
+        }
 
     }
 
